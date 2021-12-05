6982efa2b546b43982ae9b8702604921e675a3cd
Lyouwm <2393599906@qq.com>
2021-12-05 14:18:32 +0800

    指针题

--- 指针题/指针题/test.c
+++ 指针题/指针题/test.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include <string.h>
+#include<assert.h>
 
 //int main()
 //{
@@ -148,27 +149,27 @@
 
 
 
-struct Test
-{
-	int Num;
-	char *pcName;
-	short sDate;
-	char cha[2];
-	short sBa[4];
-}* p;
+//struct Test
+//{
+//	int Num;
+//	char *pcName;
+//	short sDate;
+//	char cha[2];
+//	short sBa[4];
+//}* p;
+//
+////假设p 的值为0x100000。 如下表表达式的值分别为多少？
+////已知，结构体Test类型的变量大小是20个字节
 
-//假设p 的值为0x100000。 如下表表达式的值分别为多少？
-//已知，结构体Test类型的变量大小是20个字节
-
-int main()
-{
-	p = (struct Test*)0x100000;
-	printf("%p\n", p + 0x1);
-	printf("%p\n", (unsigned long)p + 0x1);
-	printf("%p\n", (unsigned int*)p + 0x1);
-
-	return 0;
-}
+//int main()
+//{
+//	p = (struct Test*)0x100000;
+//	printf("%p\n", p + 0x1);
+//	printf("%p\n", (unsigned long)p + 0x1);
+//	printf("%p\n", (unsigned int*)p + 0x1);
+//
+//	return 0;
+//}
 
 
 
@@ -263,3 +264,218 @@ int main()
 //	printf("%s\n", cpp[-1][-1] + 1);
 //	return 0;
 //}
+
+
+
+////****************************************************************888
+////****************************************************************888
+//作业
+
+//int main()
+//{
+//	unsigned long pulArray[] = { 6, 7, 8, 9, 10 };
+//	unsigned long *pulPtr;
+//
+//	pulPtr = pulArray;
+//	*(pulPtr + 3) += 3;
+//
+//	printf("%d, %d\n", *pulPtr, *(pulPtr + 3));
+//
+//	return 0;
+//}
+
+
+
+
+
+
+//void reserve_str(char* str,int len)
+//{
+//	assert(str);
+//	char* left = str;
+//	char* right = str+len - 1;
+//	while (left<right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char arr[256] = { 0};
+//	gets(arr);
+//	//scanf("%s",arr);
+//	int len=strlen(arr);
+//	printf("字符串：%s\n", arr);
+//	
+//	reserve_str(arr,len);
+//	printf("逆序之后字符串：%s", arr);
+//}
+
+
+
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	scanf("%d%d",&a,&n);
+//	int sum = 0;
+//	int i = 0;
+//	int ret = 0;
+//	//2 5   2 22 222 2222 22222
+//	for ( i = 0; i < n; i++)
+//	{
+//		ret = ret * 10 + a;
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//}
+
+
+
+
+
+
+//#include<math.h>
+//int main()
+//{
+//	    int i = 0;
+//		for (i = 0; i <= 100000; i++)
+//		{
+//			//判断i是否为水仙花数（自幂数）
+//			//1. 计算i的位数 - n位数
+//			int n = 1;
+//			int tmp = i;
+//			int sum = 0;
+//			while (tmp /= 10)
+//			{
+//				n++;
+//			}
+//			//2. 计算i的每一位的n次方之和 sum
+//			tmp = i;
+//			while (tmp)
+//			{
+//				sum += (int)pow(tmp % 10, n);
+//				tmp /= 10;
+//			}
+//			//3. 比较i == sum
+//			if (i == sum)
+//			{
+//				printf("%d ", i);
+//			}
+//		}
+//
+//}
+
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+//*************
+// ***********
+//  *********
+//   *******
+//    *****
+//     ***
+//      *
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	//打印上半部分
+//	int i = 0;
+//	for ( i = 0; i < line; i++)
+//	{
+//		//打印空格
+//		int j = 0;
+//		for ( j = 0; j < line-i-1; j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印*
+//		for (j = 0; j < 2*i+1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	//打印下半部分
+//	for ( i = 0; i < line-1; i++)
+//	{
+//		//打印空格
+//		int j = 0;
+//		for(j=0;j<i+1;j++)
+//		{
+//			printf(" ");
+//		}
+//		//打印*
+//		for (j = 0; j < 2*(line-1-i)-1;j++ )
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//}
+
+
+
+//struct stu
+//{
+//	int num;
+//	char name[10];
+//	int age;
+//};
+//
+//void fun(struct stu *p)
+//{
+//	printf("%s\n", (*p).name);
+//	return;
+//}
+//
+//int main()
+//{
+//	struct stu students[3] = { { 9801, "zhang", 20 },
+//	                           { 9802, "wang", 19 },
+//							   { 9803, "zhao", 18 }
+//							 };
+//	fun(students + 1);
+//	return 0;
+//}
+
+
+
+
+
+//
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）。
+//0.5
+//20/0.5 = 40-1 = 39
+//
+int main()
+{
+	int money = 0;
+	int total = 0;
+	int empty = 0;
+	scanf("%d",&money);
+	//买回来的
+	total = money;
+	empty = money;
+	//换回来的
+	while (empty>=2)
+	{
+		total = total + empty / 2;
+		empty = empty / 2 + empty % 2;
+
+	}
+	printf("%d\n",total);
+	return 0;
+}
